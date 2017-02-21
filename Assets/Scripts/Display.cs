6bfbe38e9d691b4bcf054a3a7b6673c01b876879
Hantlowt <lhote.antoine@gmail.com>
2017-02-21 09:50:30 +0100

    Fix a fail on push Display.cs

--- Assets/Scripts/Display.cs
+++ Assets/Scripts/Display.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Display : MonoBehaviour {
+public class Display : MonoBehaviour
+{
     public GameObject[] led_images_prefab;
     private GameObject[] led_images;
-    private int pos;
     private int nbr_instance;
     public float speed;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         nbr_instance = 4;
         led_images = new GameObject[nbr_instance];
-        pos = 0;
         for (int i = 0; i < nbr_instance; i++)
         {
             led_images[i] = Instantiate(led_images_prefab[i % led_images_prefab.Length]);
@@ -20,10 +20,10 @@ public class Display : MonoBehaviour {
             led_images[i].transform.localPosition = new Vector3(4.05f, 0.0f, -0.1f);
         }
     }
-	
+
     void MoveImages()
     {
-        for(int i = 0; i < nbr_instance; i++)
+        for (int i = 0; i < nbr_instance; i++)
         {
             if (led_images[i].transform.localPosition.x > -2.2f)
             {
@@ -41,8 +41,9 @@ public class Display : MonoBehaviour {
                 led_images[i].transform.localPosition = new Vector3(4.05f, 0.0f, -0.1f);
         }
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         MoveImages();
         /*if (led_images[2] != null &&
             led_images[MathMod(pos - 1, 5)].transform.position.x <= 1.85f)
@@ -51,27 +52,11 @@ public class Display : MonoBehaviour {
             led_images[pos].transform.parent = this.transform;
             led_images[pos].transform.position = new Vector3(4.05f, 0.0f, -0.1f);
         }*/
-		
-	}
+
+    }
 
     static int MathMod(int a, int b)
     {
         return (System.Math.Abs(a * b) + a) % b;
     }
 }
-
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Display : MonoBehaviour {
-    public GameObject[] led_images_prefab;
-    private GameObject[] led_images;
-    private int nbr_instance;
-    public float speed;
-	// Use this for initialization
-        led_images = new GameObject[nbr_instance];
-        for (int i = 0; i < nbr_instance; i++)
-        {
-            led_images[i] = Instantiate(led_images_prefab[i % led_images_prefab.Length]);
-            led_images[i].transform.parent = this.transform;
