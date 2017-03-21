f84becff65c74ac03cfc7ca08cf46d21a8331777
Liliaze <diana.boudy@gmail.com>
2017-03-21 11:49:02 +0100

    some fix

--- Assets/Scripts/GraphKeywords1.cs
+++ Assets/Scripts/GraphKeywords1.cs
@@ -5,38 +5,50 @@ using HoloToolkit.Unity.InputModule;
 
 public class GraphKeywords1 : MonoBehaviour, ISpeechHandler
 {
-	private Color objectColor;
 	public GameObject myGraphChild;
 
 	public void ChangeColorGraphLine (string color)
 	{
 		GraphLine g = myGraphChild.GetComponent<GraphLine>();
-		objectColor = g.GetComponent<MeshRenderer>().material.color;
+		Color objectColor = g.GetComponent<MeshRenderer>().material.color;
 		if (g.online) //cas connecté au serveur
 		{
 			SyncGraphLine sync = myGraphChild.transform.parent.GetComponent<DefaultSyncModelAccessor>().SyncModel as SyncGraphLine;
-			switch (color.ToLower())
+			switch (color)
 			{
 				case "red":
-					sync.Color_R.Value = Color.red.r;
-					sync.Color_G.Value = Color.red.g;
-					sync.Color_B.Value = Color.red.b;
-					break;
+					{
+						sync.Color_R.Value = Color.red.r;
+						sync.Color_G.Value = Color.red.g;
+						sync.Color_B.Value = Color.red.b;
+						break;
+					}
 				case "blue":
-					sync.Color_R.Value = Color.blue.r;
-					sync.Color_G.Value = Color.blue.g;
-					sync.Color_B.Value = Color.blue.b;
-					break;
+					{
+						sync.Color_R.Value = Color.blue.r;
+						sync.Color_G.Value = Color.blue.g;
+						sync.Color_B.Value = Color.blue.b;
+						break;
+					}
 				case "green":
-					sync.Color_R.Value = Color.green.r;
-					sync.Color_G.Value = Color.green.g;
-					sync.Color_B.Value = Color.green.b;
-					break;
+					{
+						sync.Color_R.Value = Color.green.r;
+						sync.Color_G.Value = Color.green.g;
+						sync.Color_B.Value = Color.green.b;
+						break;
+					}
+				case "yellow":
+					{
+						sync.Color_R.Value = Color.yellow.r;
+						sync.Color_G.Value = Color.yellow.g;
+						sync.Color_B.Value = Color.yellow.b;
+						break;
+					}
 			}
 		}
 		else //non connecté changement couleur pour le graph en ligne
 		{
-			switch (color.ToLower())
+			switch (color)
 			{
 				case "red":
 					objectColor = Color.red;
@@ -46,6 +58,9 @@ public class GraphKeywords1 : MonoBehaviour, ISpeechHandler
 					break;
 				case "green":
 					objectColor = Color.green;
+					break;
+				case "yellow":
+					objectColor = Color.yellow;
 					break;
 			}
 		}
@@ -57,28 +72,41 @@ public class GraphKeywords1 : MonoBehaviour, ISpeechHandler
 		if (g.online) //cas connecté au serveur
 		{
 			SyncGraphBar sync = myGraphChild.transform.parent.GetComponent<DefaultSyncModelAccessor>().SyncModel as SyncGraphBar;
-			switch (color.ToLower())
+			switch (color)
 			{
 				case "red":
-					sync.Color_R.Value = Color.red.r;
-					sync.Color_G.Value = Color.red.g;
-					sync.Color_B.Value = Color.red.b;
-					break;
+					{
+						sync.Color_R.Value = Color.red.r;
+						sync.Color_G.Value = Color.red.g;
+						sync.Color_B.Value = Color.red.b;
+						break;
+					}
 				case "blue":
-					sync.Color_R.Value = Color.blue.r;
-					sync.Color_G.Value = Color.blue.g;
-					sync.Color_B.Value = Color.blue.b;
-					break;
+					{
+						sync.Color_R.Value = Color.blue.r;
+						sync.Color_G.Value = Color.blue.g;
+						sync.Color_B.Value = Color.blue.b;
+						break;
+					}
 				case "green":
-					sync.Color_R.Value = Color.green.r;
-					sync.Color_G.Value = Color.green.g;
-					sync.Color_B.Value = Color.green.b;
-					break;
+					{
+						sync.Color_R.Value = Color.green.r;
+						sync.Color_G.Value = Color.green.g;
+						sync.Color_B.Value = Color.green.b;
+						break;
+					}
+				case "yellow":
+					{
+						sync.Color_R.Value = Color.yellow.r;
+						sync.Color_G.Value = Color.yellow.g;
+						sync.Color_B.Value = Color.yellow.b;
+						break;
+					}
 			}
 		}
 		else // non connecté changement couleur pour le graph en bar
 		{
-			switch (color.ToLower())
+			switch (color)
 			{
 				case "red":
 					g.ColorBar = Color.red;
@@ -89,40 +117,45 @@ public class GraphKeywords1 : MonoBehaviour, ISpeechHandler
 				case "green":
 					g.ColorBar = Color.green;
 					break;
+				case "yellow":
+					g.ColorBar = Color.yellow;
+					break;
 			}
 		}
 	}
 
-	public void DestroyGameObject ()
+	public void DestroyGameObject (GameObject myObject)
 	{
-		Destroy(myGraphChild.transform.parent);
+		Destroy(myObject, 0.1f);
 	}
 
-	public void HideObject ()
+	public void HideObject (GameObject myObject)
 	{
-		if (myGraphChild.activeSelf)
-			myGraphChild.SetActive(false);
+		if (myObject.activeSelf)
+			myObject.SetActive(false);
 		else
-			myGraphChild.SetActive(true);
+			myObject.SetActive(true);
 	}
 
 	public void OnSpeechKeywordRecognized (SpeechKeywordRecognizedEventData eventData)
 	{
+		print("vous avez dis :" + eventData.RecognizedText);
 		switch (eventData.RecognizedText.ToLower())
 		{
 			case "close":
-				DestroyGameObject();
+				DestroyGameObject(gameObject);
 				break;
 			case "hide":
-				HideObject();
+				HideObject(myGraphChild);
 				break;
 			default:
-				if (myGraphChild.GetComponent<MeshRenderer>())
-					ChangeColorGraphLine(eventData.RecognizedText);
-				else
-					ChangeColorGraphBar(eventData.RecognizedText);
-				break;
+				{
+					if (myGraphChild.GetComponent<MeshRenderer>())
+						ChangeColorGraphLine(eventData.RecognizedText.ToLower());
+					else
+						ChangeColorGraphBar(eventData.RecognizedText.ToLower());
+					break;
+				}
 		}
-		print("vous avez dis :" + eventData.RecognizedText);
 	}
 }
