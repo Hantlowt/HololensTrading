bc3c6083a0d588ff6ed5e6e5b7f232cc38741873
Liliaze <diana.boudy@gmail.com>
2017-04-04 17:10:23 +0200

    upgrade menu whiteboard

---
+++ Assets/Scripts/WhiteBoard/zoomGui.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomGui : MonoBehaviour {
+
+	public void zoomIn()
+	{
+		GetComponent<Transform>().localScale = new Vector3(GetComponent<Transform>().localScale.x * 2, GetComponent<Transform>().localScale.y * 2, GetComponent<Transform>().localScale.z);
+	}
+
+	public void zoomOut ()
+	{
+		GetComponent<Transform>().localScale = new Vector3(GetComponent<Transform>().localScale.x / 2, GetComponent<Transform>().localScale.y / 2, GetComponent<Transform>().localScale.z);
+	}
+}
