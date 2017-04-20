8912cf365c7bf2c9103953016a4986fd92f7b6d1
Hantlowt <lhote.antoine@gmail.com>
2017-04-20 16:57:12 +0200

    WIP spawn + distant server

---
+++ Assets/Scripts/MoveIfCollide.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Sharing;
+using HoloToolkit.Sharing.Spawning;
+
+public class MoveIfCollide : MonoBehaviour {
+
+    SharePosition s;
+	// Use this for initialization
+	void Start () {
+        s = GetComponent<SharePosition>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+        if (collisionInfo.tag == "Graph")
+            s.receive_data = false;
+    }
+
+    void OnTriggerStay(Collider collisionInfo)
+    {
+        if (collisionInfo.tag == "Graph")
+            this.transform.localPosition += new Vector3(0.3f, 0.0f, 0.0f);
+    }
+
+    void OnTriggerExit(Collider collisionInfo)
+    {
+        if (collisionInfo.tag == "Graph")
+        {
+            SyncSpawnedObject sync = GetComponent<DefaultSyncModelAccessor>().SyncModel
+                as SyncSpawnedObject;
+            sync.Position.Value = transform.position;
+            sync.Rotation.Value = transform.rotation;
+            sync.Scale.Value = transform.localScale;
+            s.receive_data = true;
+        }
+    }
+}
