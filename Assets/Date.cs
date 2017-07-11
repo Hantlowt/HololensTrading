ebd20e1e00e574736f1444a5f26137310e7211bd
Hantlowt <lhote.antoine@gmail.com>
2017-07-11 13:51:33 +0200

    Update

---
+++ Assets/Date.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Date : MonoBehaviour {
+
+    TextMeshPro textMesh;
+	// Use this for initialization
+	void Start () {
+        textMesh = GetComponent<TextMeshPro>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textMesh.text = DateTime.Now.ToString();
+	}
+}
